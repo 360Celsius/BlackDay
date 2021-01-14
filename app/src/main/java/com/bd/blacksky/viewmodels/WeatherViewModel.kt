22cd11edce092becaa45c238333784a0d0ee5aa5
Dennis Shar <dennis.shar@gmail.com>
2021-01-14 09:34:38 +0200

    Update WeatherViewModel.kt

--- app/src/main/java/com/bd/blacksky/viewmodels/WeatherViewModel.kt
+++ app/src/main/java/com/bd/blacksky/viewmodels/WeatherViewModel.kt
@@ -2,6 +2,8 @@ package com.bd.blacksky.viewmodels
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.bd.blacksky.data.database.entities.CurrentWeatherEntity
+import com.bd.blacksky.data.database.entities.WeeklyDayWeatherEntity
 import com.bd.blacksky.data.network.datamodels.WeeklyWeatherDataModel
 import com.bd.blacksky.repositories.WeatherRepository
 import com.bd.blacksky.utils.Coroutines
@@ -15,9 +17,37 @@ class WeatherViewModel(
         Coroutines.backGround {
             try{
                 val weatherResponce: Response<WeeklyWeatherDataModel>  = weatherRepository.getWeatherFromAPI(lat,lon,appid)
-                Log.e("test", "timezone "+ weatherResponce.body()?.timezone.toString())
+
+                val currurentWeatherEntityRandomId: Int =  (0..100).random()
+                val currentWeatherEntity: CurrentWeatherEntity = CurrentWeatherEntity(
+                    0,
+                        currurentWeatherEntityRandomId,
+                        weatherResponce.body()?.current?.temp,
+                        weatherResponce.body()?.current?.wind_speed,
+                        weatherResponce.body()?.current?.weather?.get(0)?.description,
+                        weatherResponce.body()?.current?.weather?.get(0)?.main
+                )
+
+                weatherRepository.saveCurrentWeatherToDB(currentWeatherEntity)
+
+
+                val weeklyArraySize: Int? =  weatherResponce.body()?.daily?.size?.minus(1)
+                for(i in 0..(weeklyArraySize?:0)){
+                    val weeklyDayWeatherEntity: WeeklyDayWeatherEntity = WeeklyDayWeatherEntity(
+                            i,
+                            currurentWeatherEntityRandomId,
+                            weatherResponce.body()?.daily?.get(i)?.dt,
+                            weatherResponce.body()?.daily?.get(i)?.temp?.min,
+                            weatherResponce.body()?.daily?.get(i)?.temp?.max,
+                            weatherResponce.body()?.daily?.get(i)?.weather?.get(0)?.description.toString(),
+                            weatherResponce.body()?.daily?.get(i)?.weather?.get(0)?.main.toString()
+                    )
+                    weatherRepository.saveWeeklyDayWeatherToDB(weeklyDayWeatherEntity)
+                }
+
+                Log.e("test", "WeatherViewModel "+ weatherResponce.body()?.timezone.toString())
             }catch (e: Exception){
-                Log.e("test", e.toString())
+                Log.e("Coroutines Error", e.toString())
             }
         }
     }
