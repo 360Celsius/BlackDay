35f45b1401c5db205f32b334355a6e7b75b729a4
Dennis Shar <dennis.shar@gmail.com>
2021-01-20 19:57:54 +0200

    getUnsafeOkHttpClient added to WeatherAPI

--- app/src/main/java/com/bd/blacksky/data/network/WeatherAPI.kt
+++ app/src/main/java/com/bd/blacksky/data/network/WeatherAPI.kt
@@ -2,11 +2,19 @@ package com.bd.blacksky.data.network
 
 import com.bd.blacksky.data.network.datamodels.GeoLocationDataModel
 import com.bd.blacksky.data.network.datamodels.WeeklyWeatherDataModel
+import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.security.cert.CertificateException
+import kotlin.jvm.Throws
 
 interface WeatherAPI {
 
@@ -23,8 +31,48 @@ interface WeatherAPI {
             return Retrofit.Builder()
                     .baseUrl("https://api.openweathermap.org/data/2.5/")
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(getUnsafeOkHttpClient())
                     .build()
                     .create(WeatherAPI::class.java)
+        }
+
+        open fun getUnsafeOkHttpClient(): OkHttpClient? {
+            return try {
+                // Create a trust manager that does not validate certificate chains
+                val trustAllCerts = arrayOf<TrustManager>(
+                    object : X509TrustManager {
+                        @Throws(CertificateException::class)
+                        override fun checkClientTrusted(
+                            chain: Array<X509Certificate?>?,
+                            authType: String?
+                        ) {
+                        }
+
+                        @Throws(CertificateException::class)
+                        override fun checkServerTrusted(
+                            chain: Array<X509Certificate?>?,
+                            authType: String?
+                        ) {
+                        }
+
+                        override fun getAcceptedIssuers(): Array<X509Certificate?>? {
+                            return arrayOf()
+                        }
+                    }
+                )
+
+                // Install the all-trusting trust manager
+                val sslContext = SSLContext.getInstance("SSL")
+                sslContext.init(null, trustAllCerts, SecureRandom())
+                // Create an ssl socket factory with our all-trusting manager
+                val sslSocketFactory = sslContext.socketFactory
+                val builder = OkHttpClient.Builder()
+                builder.sslSocketFactory(sslSocketFactory)
+                builder.hostnameVerifier { hostname, session -> true }
+                builder.build()
+            } catch (e: Exception) {
+                throw RuntimeException(e)
+            }
         }
     }
 }
